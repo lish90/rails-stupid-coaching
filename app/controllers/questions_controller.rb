65ec6e6d57feb34ecada33cf931bac4a06fd14f3
alisha musamabik <alisha.musamabik@gmail.com>
2020-01-26 00:27:00 +0000

    frontend form update with css

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-      if @question = 'I am going to work'
+      if @question == 'I am going to work'
         @answer = 'Great!'
       elsif @question.include?('?')
         @answer = 'Silly question, get dressed and go to work!'
